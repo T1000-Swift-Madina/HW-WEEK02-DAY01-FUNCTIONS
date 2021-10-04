ec30b811c95a9f6e0641f05b483c2345ffecea25
wasaif89 <ahmedmansour3@hotmail.com>
2021-10-04 08:30:48 +0300

    hw

---
+++ hw1wee2.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+import UIKit
+
+func  SelectCity(arrayOfCities:[String],Index: Int) -> String{
+
+    var CityName :String;
+
+    if(Index > arrayOfCities.count-1 || Index < 0 ){
+
+        CityName = "No City Found"
+    }
+    else{
+
+        CityName = arrayOfCities[Index]
+
+    }
+
+    return CityName
+}
+
+
+print(SelectCity(arrayOfCities: ["city1","city2","city3"], Index: 1))

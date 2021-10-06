f6459b4d47b3b2a43c61275b90a4e4aa941bee22
reem <reem.saleh.med.s@tuwaiq.edu.sa>
2021-10-06 16:09:10 +0300

    HW

---
+++ func.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+ let ourcity = ["Medina","Hail","Tabuk"]
+ func Iistcity(ourcity : [String] ,index :Int ) -> String{
+     if index < 0 {return "not"}
+     if index > ourcity.count - 1 {
+ return"not found"}
+
+      return ourcity[index]
+     }
+
+     print(Iistcity(ourcity: ourcity, index :1))

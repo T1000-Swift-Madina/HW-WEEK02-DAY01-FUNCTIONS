f2b0518609d356209c3bc3528dfe9725ebe61482
reem alsubhi <Reem.Alsubi.med.s@tuwaiq.edu.sa>
2021-10-06 16:22:03 +0300

    city2

---
+++ citty.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var greeting = "Hello, playground"
+ 
+
+
+
+func getCityByIndex(cities: [String], selectedIndex: Int) -> String
+{
+    return cities[selectedIndex]
+}
+
+getCityByIndex(cities: ["ruh", "jed"], selectedIndex: 1)
+    
+
